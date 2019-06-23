f52f66c0e21453fc6a814efae4259d69d7ab7003
Kevin Conner <connerk@gmail.com>
2019-06-23 09:34:38 -0400

    Start with the Single View App template

---
+++ Primitives/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Primitives
+//
+//  Created by Kevin Conner on 6/23/19.
+//  Copyright © 2019 Kevin Conner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ PrimitivesTests/PrimitivesTests.swift
@@ -0,0 +1,34 @@
+//
+//  PrimitivesTests.swift
+//  PrimitivesTests
+//
+//  Created by Kevin Conner on 6/23/19.
+//  Copyright © 2019 Kevin Conner. All rights reserved.
+//
+
+import XCTest
+@testable import Primitives
+
+class PrimitivesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
